f18d9ec47455391e6257c9537c534023220ca09c
chancandan <daniellyons@live.ie>
2024-04-16 21:54:41 +0000

    Add products app, models and fixtures

--- the_cheese_shoppe/settings.py
+++ the_cheese_shoppe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-**4uf0t2--fh@y8s9-nd!#-gz&ggy*-6cdpu*-+pxfr%7z&*x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-chancandan-thecheesesho-io4ghq68261.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-chancandan-thecheesesho-p2xxqb6b0o8.ws-eu110.gitpod.io']
 
 
 # Application definition
