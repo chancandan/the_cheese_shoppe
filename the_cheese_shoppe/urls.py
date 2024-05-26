26f91f1a57fabc8f779e6830fa371b4c5c99d4da
chancandan <daniellyons@live.ie>
2024-05-26 20:35:55 +0000

    Create a 404.html page and update relvant views and urls

--- the_cheese_shoppe/urls.py
+++ the_cheese_shoppe/urls.py
@@ -1,22 +1,9 @@
-"""the_cheese_shoppe URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from the_cheese_shoppe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +14,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = views.custom_404
