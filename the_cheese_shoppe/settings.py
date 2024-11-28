38c8792d69e7b4d368edf5f716c01a37baceb818
chancandan <daniellyons@live.ie>
2024-11-28 22:53:14 +0000

    Testing email sending

--- the_cheese_shoppe/settings.py
+++ the_cheese_shoppe/settings.py
@@ -240,7 +240,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    EMFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
     #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     #EMAIL_HOST = 'smtp.mailersend.net'
